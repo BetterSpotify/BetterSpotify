94b42476511d32d333e1f033be1d19dc27257120
Seth Stephens <relativexd@protonmail.com>
2018-10-03 23:19:45 -0400

    build changes

--- client/src/init.js
+++ client/src/init.js
@@ -1,9 +1,10 @@
 const Util = require("./util");
 
 module.exports = () => {
-  let link = document.createElement("link");
+  /*let link = document.createElement("link");
   link.rel = "stylesheet";
   link.href = "http://localhost:8161/stylesheet.css";
   document.getElementsByTagName("head")[0].appendChild(link);
-  Util.lastInjectedStyle = link;
+  Util.lastInjectedStyle = link;*/
+  Util.injectStyles(__BUILDTIME_STYLES__);
 };
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,9 @@
 const webpack = require("webpack"),
-  path = require("path");
+  path = require("path"),
+  fs = require("fs");
 const DIST_DIRECTORY = path.resolve(__dirname, "..", "dist");
+const STYLE_PATH = path.join(DIST_DIRECTORY, "stylesheet.css");
+
 module.exports = {
   module: {
     rules: [
@@ -19,5 +22,10 @@ module.exports = {
   output: {
     path: DIST_DIRECTORY,
     filename: "injector.js"
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __BUILDTIME_STYLES__: `\`${fs.readFileSync(STYLE_PATH, "utf8")}\``
+    })
+  ]
 };
